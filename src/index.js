5c97d45f245aee77bf3556619858ec15463f9cda
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-05-09 10:50:26 +0530

    Server index file initiated and added

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectToDatabase from "./database_connections/index.database_connections.js";
+
+dotenv.config({
+    path: "./env"
+});
+
+connectToDatabase()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port http://localhost:${process.env.PORT} & DB Connection Successful`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error in connecting to database and starting the server" + error);
+        process.exit(1);
+    });
