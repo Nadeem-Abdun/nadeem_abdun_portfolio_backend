038daa840eb7215cbccf87996653a734a74a95d0
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-05-24 21:52:37 +0530

    WallOfCode Entity Controller Updated

--- src/controllers/wallOfCode.controllers.js
+++ src/controllers/wallOfCode.controllers.js
@@ -1,3 +1,4 @@
+import Profile from "../models/profile.models.js";
 import WallOfCode from "../models/wallOfCode.models.js";
 import asyncHandler from "../utilities/AsyncHandler.utilities.js";
 import ApiResponse from "../utilities/ApiResponse.utilities.js";
@@ -28,7 +29,7 @@ const updateSkill = asyncHandler(async (req, res) => {
     if (!id) { throw new ApiError(400, "Skill Id is required, Please provide skill id to continue.") }
     if (!skillName) { throw new ApiError(400, "Skill Name is required, Please provide skill name to continue.") }
     if (!skillIcon) { throw new ApiError(400, "Skill Icon is required, Please provide skill icon to continue.") }
-    const payload = await WallOfCode.findOneAndUpdate(id, {
+    const payload = await WallOfCode.findByIdAndUpdate(id, {
         skillName: skillName,
         skillIcon: skillIcon
     }, { new: true });
@@ -40,7 +41,7 @@ const updateSkill = asyncHandler(async (req, res) => {
 
 const deleteSkill = asyncHandler(async (req, res) => {
     const { id } = req.params;
-    if (!id) { throw new ApiError(400, "Skill Id is required, Please provide skill id to continue.") }
+    if (!id) { throw new ApiError(400, "Skill id is required, Please provide skill id to continue.") }
     const payload = await WallOfCode.findByIdAndDelete(id);
     if (!payload) { throw new ApiError(500, "Error in deleting the skill, Please try again.") }
     const updateProfile = await Profile.findByIdAndUpdate(payload.profileId, { $pull: { listOfWallOfCodeSkills: payload._id } }, { new: true });
@@ -53,7 +54,7 @@ const deleteSkill = asyncHandler(async (req, res) => {
 const getSkills = asyncHandler(async (req, res) => {
     const { profileId } = req.params;
     if (!profileId) { throw new ApiError(400, "Profile Id is required, Please provide peofile id to continue.") }
-    const payload = await WallOfCode.find(profileId);
+    const payload = await WallOfCode.find({ profileId: profileId });
     if (!payload) { throw new ApiError(500, "Error in getting the skills, Please try again.") }
     return res
         .status(200)
