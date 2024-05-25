04ef223edcc883620bbc0192dc84c525417b1efa
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-05-25 21:33:01 +0530

    User Entity Route Added

---
+++ src/routes/user.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, updatePassword, getLoggedInUser, updateAccountDetails, getPortfolioProfile } from "../controllers/user.controllers.js";
+import verifyUserAccess from "../middlewares/Authentication.middlewares.js";
+
+const router = Router();
+
+// Un-Protected Routes
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/get-profile/:username").get(getPortfolioProfile);
+
+// Protected Routes
+router.route("/logout").post(verifyUserAccess, logoutUser);
+router.route("/update-password").post(verifyUserAccess, updatePassword);
+router.route("/get-user").get(verifyUserAccess, getLoggedInUser);
+router.route("/update-account").get(verifyUserAccess, updateAccountDetails);
+
+export default router;
