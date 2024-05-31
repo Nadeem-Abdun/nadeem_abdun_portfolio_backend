96d365623cf311b994fb4719ed690819b3fde950
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-05-31 21:55:15 +0530

    Profile Entity Route Updated

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createProfile, updateProfile, deleteProfile, getProfile } from "../controllers/profile.controllers.js";
 import verifyUserAccess from "../middlewares/Authentication.middlewares.js";
+import upload from "../middlewares/Multer.middlewares.js";
 
 const router = Router();
 
@@ -8,8 +9,8 @@ const router = Router();
 router.route("/get/:profileId").get(getProfile);
 
 // Protected Routes
-router.route("/add").post(verifyUserAccess, createProfile);
-router.route("/update/:profileId").put(verifyUserAccess, updateProfile);
+router.route("/create").post(verifyUserAccess, upload.single("profilePicture"), createProfile);
+router.route("/update/:profileId").put(verifyUserAccess, upload.single("profilePicture"), updateProfile);
 router.route("/delete/:profileId").delete(verifyUserAccess, deleteProfile);
 
 export default router;
