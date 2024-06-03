2aa262fb2eabf35650017f558c5bb8cce1404a68
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-06-04 00:02:34 +0530

    Bug Fix

--- src/utilities/Cloudinary.utilities.js
+++ src/utilities/Cloudinary.utilities.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs";
-import { CLOUDINARY_FOLDER_NAME } from "./constants.utilities";
+import { CLOUDINARY_FOLDER_NAME } from "./constants.utilities.js";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
