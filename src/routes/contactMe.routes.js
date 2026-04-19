62567054c514886cc6f967993a30afc8165393bc
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2026-04-19 17:46:38 +0530

    Health Check Added, Bugs Fixed

--- src/routes/contactMe.routes.js
+++ src/routes/contactMe.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createContactForm, getContactForms, replyContactForm, deleteContactForm } from "../controllers/contactMe.controllers.js";
 import verifyUserAccess from "../middlewares/Authentication.middlewares.js";
+import { contactFormRateLimiter } from "../middlewares/RateLimiter.middlewares.js";
+import { validateContactForm } from "../middlewares/Validation.middlewares.js";
 
 const router = Router();
 
 // Un-Protected Routes
-router.route("/create/:profileId").post(createContactForm);
+router.route("/create/:profileId").post(contactFormRateLimiter, validateContactForm, createContactForm);
 
 // Protected Routes
 router.route("/getForms/:profileId").get(verifyUserAccess, getContactForms);
