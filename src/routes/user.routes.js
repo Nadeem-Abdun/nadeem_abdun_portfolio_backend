d340e6798bb7c17c64dc0fd3dbf3b21de2c9baab
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2026-04-21 17:47:04 +0530

    Health Check Added, Bugs Fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { registerUser, loginUser, logoutUser, refreshAccessToken, updatePassword, getLoggedInUser, updateAccountDetails, getPortfolioProfile } from "../controllers/user.controllers.js";
 import verifyUserAccess from "../middlewares/Authentication.middlewares.js";
+import { authRateLimiter } from "../middlewares/RateLimiter.middlewares.js";
+import { validateRegistration, validateLogin } from "../middlewares/Validation.middlewares.js";
 
 const router = Router();
 
 // Un-Protected Routes
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
+router.route("/register").post(authRateLimiter, validateRegistration, registerUser);
+router.route("/login").post(authRateLimiter, validateLogin, loginUser);
 router.route("/refresh-token").get(refreshAccessToken);
 router.route("/get-profile/:username").get(getPortfolioProfile);
 
@@ -14,6 +16,6 @@ router.route("/get-profile/:username").get(getPortfolioProfile);
 router.route("/logout").get(verifyUserAccess, logoutUser);
 router.route("/update-password").post(verifyUserAccess, updatePassword);
 router.route("/get-user").get(verifyUserAccess, getLoggedInUser);
-router.route("/update-account").get(verifyUserAccess, updateAccountDetails);
+router.route("/update-account").patch(verifyUserAccess, updateAccountDetails);
 
 export default router;
